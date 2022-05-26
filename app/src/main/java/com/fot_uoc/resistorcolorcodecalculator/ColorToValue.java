6253b23fb3834b84680f698474be28801a4f8792
rjchamara <2019t00375@stu.cmb.ac.lk>
2022-05-26 20:03:31 +0530

    I added a 1st band drop down list

--- app/src/main/java/com/fot_uoc/resistorcolorcodecalculator/ColorToValue.java
+++ app/src/main/java/com/fot_uoc/resistorcolorcodecalculator/ColorToValue.java
@@ -3,12 +3,34 @@ package com.fot_uoc.resistorcolorcodecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class ColorToValue extends AppCompatActivity {
+    String[] colors = {"Black","Brown"};
+    AutoCompleteTextView band01;
+    ArrayAdapter<String> adaptercolors;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_to_value);
+
+        band01= findViewById(R.id.band1);
+        adaptercolors= new ArrayAdapter<String>(this,R.layout.bands,colors);
+        band01.setAdapter(adaptercolors);
+        band01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), "color: "+item, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
