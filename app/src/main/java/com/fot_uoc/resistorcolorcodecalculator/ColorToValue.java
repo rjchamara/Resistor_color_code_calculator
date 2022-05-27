ac43dfe7b3faf0108b77f4b02ee618ac50a3a08c
rjchamara <2019t00375@stu.cmb.ac.lk>
2022-05-27 07:02:53 +0530

    I modified 1st band code

--- app/src/main/java/com/fot_uoc/resistorcolorcodecalculator/ColorToValue.java
+++ app/src/main/java/com/fot_uoc/resistorcolorcodecalculator/ColorToValue.java
@@ -2,35 +2,65 @@ package com.fot_uoc.resistorcolorcodecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ColorToValue extends AppCompatActivity {
-    String[] colors = {"Black","Brown"};
-    AutoCompleteTextView band01;
-    ArrayAdapter<String> adaptercolors;
-
+    //initialize variable
+    TextView band1;
+    Button band1btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_to_value);
 
-        band01= findViewById(R.id.band1);
-        adaptercolors= new ArrayAdapter<String>(this,R.layout.bands,colors);
-        band01.setAdapter(adaptercolors);
-        band01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = parent.getItemAtPosition(position).toString();
-                Toast.makeText(getApplicationContext(), "color: "+item, Toast.LENGTH_SHORT).show();
+        //assign variable
+        TextView band1 = (TextView) findViewById(R.id.band1);
+        band1btn = findViewById(R.id.band1btn);
 
+        band1btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //initialize the popup menu
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(),band1btn);
+                //inflate the popup menu
+                popupMenu.getMenuInflater().inflate(R.menu.popupmenu1,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            //set colors
+                            case R.id.none:
+                                band1.setBackgroundColor(Color.TRANSPARENT);
+                                return true;
+                            case R.id.black:
+                                band1.setBackgroundColor(Color.BLACK);
+                                return true;
+
+
+                        }
+                        return false;
+                    }
+                });
+                //show the popup menu
+                popupMenu.show();
             }
         });
+
+
+
 
     }
 }
